a9f44f340b62ed2bd7ad05e17eaa10dfcb159f3d
Sweetleo30 <diana790@inbox.ru>
2022-04-28 22:45:24 +0300

    added calculator

--- week_13/assets/js/script.js
+++ week_13/assets/js/script.js
@@ -1,10 +1,34 @@
-function showMessage() {
+function getName() {
     let name = prompt('Как тебя зовут?');
+    if (name === null) {
+        name = 'незнакомец';
+    }
     alert(`Привет, ${name}!`);
 }
 
-// showMessage();
+function getAddition() {
+    let num1 = prompt("Введите число!");
+    let num2 = prompt("Введите число!");
+    alert(Number(num1) + Number(num2));
+}
 
+function getSubtraction() {
+    let num1 = prompt("Введите число!");
+    let num2 = prompt("Введите число!");
+    alert(Number(num1) - Number(num2));
+}
+
+function getMultiplication() {
+    let num1 = prompt("Введите число!");
+    let num2 = prompt("Введите число!");
+    alert(Number(num1) * Number(num2));
+}
+
+function getDivision() {
+    let num1 = prompt("Введите число!");
+    let num2 = prompt("Введите число!");
+    alert(Number(num1) / Number(num2));
+}
 
 
 
