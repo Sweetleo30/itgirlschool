46ba87fa01c63cdfa723c52d6aae11a9544a1c16
Sweetleo30 <diana790@inbox.ru>
2022-05-17 17:10:52 +0300

    added fixes

--- week_15/assets/js/script.js
+++ week_15/assets/js/script.js
@@ -81,48 +81,96 @@ checkbox.addEventListener('change', () => {
     }
 });
 
+
 function validate() {
 
     let a = document.forms["form"]["firstname"].value;
-    if (a == "") {
-        document.getElementsByClassName('error-message')[0].innerHTML = ("Пожалуйста, укажите имя");
-    }
-
-
     let b = document.forms["form"]["lastname"].value;
-    if (b == "") {
-        document.getElementsByClassName('error-message')[1].innerHTML = ("Пожалуйста, укажите фамилию");
-    }
-
     let c = document.forms["form"]["login"].value;
-    let c_len = c.length;
-    if (c_len == 0 || c_len >= 20 || c_len < 6) {
-        document.getElementsByClassName('error-message')[2].innerHTML =
-            ("Логин должен состоять не менее чем из " + 6 + " символов и не более чем из " + 20 + " символов");
-    }
-
     let d = document.forms["form"]["password"].value;
-    let d_len = d.length;
-    if (d_len == 0 || d_len >= 12 || d_len < 7) {
-        document.getElementsByClassName('error-message')[3].innerHTML =
-            ("Пароль должен состоять не менее чем из " + 7 + " символов и не более чем из " + 12 + " символов");
-    }
-
     let e = document.forms["form"]["password_confirm"].value;
-    if (e !== d || e == "") {
-        document.getElementsByClassName('error-message')[4].innerHTML = ("Подтверждение не совпадает с паролем");
-    }
-
     let f = document.forms["form"]["phone"].value;
-    let regex = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
-    if (!regex.test(f)) {
-        document.getElementsByClassName('error-message')[5].innerHTML = ('Недопустимый формат номера');
-    }
 
+    if (firstNameValidation(a)) {
+        if (lastNameValidation(b)) {
+            if (loginValidation(c)) {
+                if (passwordValidation(d)) {
+                    if (passwordConfirmValidation(e)) {
+                        if (phoneValidation(f)) {
+                            document.getElementsByClassName('registration__message')[0].innerHTML = (`Добро пожаловать, ${a}!`);
+                            document.getElementsByClassName('registration__block')[0].style.display = 'none'
+                        }
+                    }
+                }
+            }
+        }
+    }
     return false;
 }
 
 
+function firstNameValidation(a) {
+    let errorMessage = document.getElementsByClassName('error-message')[0];
+    if (a == "") {
+        errorMessage.innerHTML = ("Пожалуйста, укажите имя");
+        return false;
+    } else {
+        return true;
+    }
+}
 
+function lastNameValidation(b) {
+    let errorMessage = document.getElementsByClassName('error-message')[1];
+    if (b == "") {
+        errorMessage.innerHTML = ("Пожалуйста, укажите фамилию");
+        return false;
+    } else {
+        return true;
+    }
+}
 
+function loginValidation(c) {
+    let errorMessage = document.getElementsByClassName('error-message')[2];
+    let c_len = c.length;
+    if (c_len == 0 || c_len >= 20 || c_len < 6) {
+        errorMessage.innerHTML =
+            ("Логин должен состоять не менее чем из " + 6 + " символов и не более чем из " + 20 + " символов");
+        return false;
+    } else {
+        return true;
+    }
+}
 
+function passwordValidation(d) {
+    let errorMessage = document.getElementsByClassName('error-message')[3];
+    let d_len = d.length;
+    if (d_len == 0 || d_len >= 12 || d_len < 7) {
+        errorMessage.innerHTML =
+            ("Пароль должен состоять не менее чем из " + 7 + " символов и не более чем из " + 12 + " символов");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function passwordConfirmValidation(e) {
+    let errorMessage = document.getElementsByClassName('error-message')[4];
+    let d = document.forms["form"]["password"].value;
+    if (e !== d || e == "") {
+        errorMessage.innerHTML = ("Подтверждение не совпадает с паролем");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function phoneValidation(f) {
+    let errorMessage = document.getElementsByClassName('error-message')[5];
+    let regex = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+    if (!regex.test(f)) {
+        errorMessage.innerHTML = ('Недопустимый формат номера');
+        return false;
+    } else {
+        return true;
+    }
+}
