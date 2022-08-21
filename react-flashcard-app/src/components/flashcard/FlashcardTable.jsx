0101ffbeec3000be92a27c00f7c7f1f2b3e477b5
Sweetleo30 <diana790@inbox.ru>
2022-08-21 21:36:31 +0300

    added input

--- react-flashcard-app/src/components/flashcard/FlashcardTable.jsx
+++ react-flashcard-app/src/components/flashcard/FlashcardTable.jsx
@@ -18,35 +18,47 @@ export function FlashcardTable() {
                         <th>Кнопки</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td scope="row">1</td>
-                        <td>olive</td>
-                        <td>[ ˈɒlɪv ]</td>
-                        <td>оливковый</td>
-                        <td>
-                            <button className="bt-save">SAVE</button>
-                            <button className="bt-edit"><FontAwesomeIcon icon={faEdit} /></button>
-                            <button className="bt-delete"><FontAwesomeIcon icon={faTrashCan} /></button>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td scope="row">1</td>
-                        <td>olive</td>
-                        <td>[ ˈɒlɪv ]</td>
-                        <td>оливковый</td>
-                        <td>
-                            <button className="bt-save">SAVE</button>
-                            <button className="bt-edit"><FontAwesomeIcon icon={faEdit} /></button>
-                            <button className="bt-delete"><FontAwesomeIcon icon={faTrashCan} /></button>
-                        </td>
-                    </tr>
-
-
-
-                </tbody>
+                <TableBody></TableBody>
             </table>
         </div>
     );
 }
+
+export function TableBody() {
+    return (
+        <tbody>
+            <tr>
+                <td scope="row">1</td>
+                <td>olive</td>
+                <td>[ ˈɒlɪv ]</td>
+                <td>оливковый</td>
+                <td>
+                    <button className="bt-edit"><FontAwesomeIcon icon={faEdit} /></button>
+                    <button className="bt-delete"><FontAwesomeIcon icon={faTrashCan} /></button>
+                </td>
+            </tr>
+
+            <tr>
+                <td scope="row">1</td>
+                <td>
+                    <input type="text" className="form-control" name="word" value="" />
+                </td>
+                <td>
+                    <input type="text" className="form-control" name="transcription" value="" />
+                </td>
+                <td>
+                    <input type="text" className="form-control" name="translation" value="" />
+                </td>
+                <td>
+                    <button className="bt-save">SAVE</button>
+                    <button className="bt-edit"><FontAwesomeIcon icon={faEdit} /></button>
+                    <button className="bt-delete"><FontAwesomeIcon icon={faTrashCan} /></button>
+                </td>
+            </tr>
+        </tbody>
+    );
+}
+
+
+
+
